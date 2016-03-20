c5586c57f20848adbbcf87e7f1586df9c92e46bb
Hegyi Bálint <baprof.hegyi@gmail.com>
2016-03-20 13:09:25 +0100

    New temporal project for PRU C++ stub developement

---
+++ pru-cpp/pru-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pru-cpp
+//
+//  Created by Hegyi Bálint on 20/03/16.
+//  Copyright © 2016 Hegyi Bálint. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
